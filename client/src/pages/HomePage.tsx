3094265803eba1a038f1fb80d30aa04c1f421fe2
TairMa23 <tairmazuz15@gmail.com>
2024-07-14 19:14:12 +0300

    Create Rating Components

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useReducer } from "react";
 import { Col, Row } from "react-bootstrap";
 import { sampleProducts } from "../data";
-import { Link } from "react-router-dom";
 import { Product } from "../types/Product";
 import axios from "axios";
 import { getError } from "../utils";
 import { ApiError } from "../types/ApiError";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import ProductItem from "../components/ProductItem";
 
 type State = {
   products: Product[];
@@ -65,15 +65,7 @@ export default function HomePage() {
     <Row>
       {sampleProducts.map((product) => (
         <Col key={product.slug} sm={6} md={4} lg={3}>
-          <Link to={"/product/" + product.slug}>
-            <img
-              src={product.image}
-              alt={product.name}
-              className="product-image"
-            ></img>
-            <h2>{product.name}</h2>
-            <p>${product.price}</p>
-          </Link>
+          <ProductItem product={product} />
         </Col>
       ))}
     </Row>
