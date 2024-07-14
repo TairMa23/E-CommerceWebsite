7fa7c3a811a959b7d3c523a8afa15a99d093ddc0
TairMa23 <tairmazuz15@gmail.com>
2024-07-14 21:40:18 +0300

    fix Page Title

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,8 +1,12 @@
 import { Helmet } from "react-helmet-async";
 
 export default function ProductPage() {
-  <Helmet>
-    <title>Product Page</title>
-  </Helmet>;
-  return <div>ProductPage</div>;
+  return (
+    <>
+      <Helmet>
+        <title>Product Page</title>
+      </Helmet>
+      <div>ProductPage</div>
+    </>
+  );
 }
