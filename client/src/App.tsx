13ea1fc99e48bd0fd24959d5f5d471b3d44caeec
TairMa23 <tairmazuz15@gmail.com>
2024-07-14 15:00:27 +0300

    createBrowserRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <header>TS Amazon</header>
-        <main></main>
-        <footer>All right reserved</footer>
+      <div className="d-flex flex-column vh-100">
+        <header>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <Navbar.Brand>tsamazona</Navbar.Brand>
+            </Container>
+            <Nav>
+              <a href="/cart" className="nav-link">
+                cart
+              </a>
+              <a href="/signin" className="nav-link">
+                Sign In
+              </a>
+            </Nav>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Outlet />
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All right reserved</div>
+        </footer>
       </div>
     </>
   );
