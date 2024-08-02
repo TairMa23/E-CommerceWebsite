ddc9e15ab6baa407bd9ad6ebf74febfebc32a15e
TairMa23 <tairmazuz15@gmail.com>
2024-08-02 16:05:07 +0300

    Run-Backend-And-Frontend-Concurrently

--- server/src/index.ts
+++ server/src/index.ts
@@ -40,9 +40,9 @@ app.use("/api/orders", orderRouter);
 app.use("/api/seed", seedRouter);
 app.use("/api/keys", keyRouter);
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 app.get("*", (req: Request, res: Response) =>
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
+  res.sendFile(path.join(__dirname, "../../client/dist/index.html"))
 );
 
 const PORT: number = parseInt((process.env.PORT || "4000") as string, 10);
